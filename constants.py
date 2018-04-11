312e196ac245e09c87b6148988043fc1da7fbf34
raymond <rzhao2145@bths.edu>
2018-04-11 15:00:31 -0400

    update constants

--- constants.py
+++ constants.py
@@ -7,7 +7,7 @@ os.environ['SDL_VIDEO_CENTERED'] = '1'
 screen_width = 500
 screen_height = 500
 screen = pygame.display.set_mode([screen_width, screen_height])
-
+pygame.display.set_caption('Boxhead')
 
 clock = pygame.time.Clock()
 
@@ -24,13 +24,15 @@ bloodpic = pygame.transform.scale(bloodpic, (50,50))
 player_up_pic = pygame.image.load(("images/player_up.png"))
 player_up_pic = pygame.transform.scale(player_up_pic, (25,25))
 
+'''                                          
+            PLAYER PICTURES
+'''
+
 player_upleft_pic = pygame.image.load(("images/player_upleft.png"))
 player_upleft_pic = pygame.transform.scale(player_upleft_pic, (25,25))
 
 player_upright_pic = pygame.image.load(("images/player_upright.png"))
 player_upright_pic = pygame.transform.scale(player_upright_pic, (25,25))
-
-
 
 player_down_pic = pygame.image.load(("images/player_down.png"))
 player_down_pic = pygame.transform.scale(player_down_pic, (25,25))
@@ -40,9 +42,6 @@ player_downleft_pic = pygame.transform.scale(player_downleft_pic, (25,25))
 
 player_downright_pic = pygame.image.load(("images/player_downright.png"))
 player_downright_pic = pygame.transform.scale(player_downright_pic, (25,25))
-
-
-
 
 player_left_pic = pygame.image.load(("images/player_left.png"))
 player_left_pic = pygame.transform.scale(player_left_pic, (25,25))
